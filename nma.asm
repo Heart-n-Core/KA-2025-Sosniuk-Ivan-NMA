daada494b1cea0046255d19c04c0da78159733ea
Ivan Sosniuk <i.sosniuk@ukma.edu.ua>
2025-03-19 21:08:02 +0200

    Read filename parameter

--- nma.asm
+++ nma.asm
@@ -2,17 +2,25 @@
 .code
 org 100h
 start:
-    call print_message
-    
+
+    call copy_param
+
     mov ax, 4C00h
     int 21h
 
-print_message proc
-    mov dx, offset msg
-    mov ah, 09h
-    int 21h
-    ret
-print_message endp
-    
-msg db '24', 0Dh, 0Ah, '$'
+copy_param proc
+    ; ds = PSP
+    mov cx, ds:[80h]
+    xor ch, ch
+    mov si, 82h        ;at offset 81h space then first char of arg
+    mov di, offset filename
+    cld
+    rep movsb
+    dec di
+    mov ds:[di], 0
+ret
+copy_param endp
+
+filename db '          ', '$'
+
 end start
